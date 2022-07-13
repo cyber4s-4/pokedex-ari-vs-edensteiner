3c9e4b2ee5da8bb89f5dea28c175c6351948b949
ari-vs <ariking006@gmail.com>
2022-07-13 21:02:22 +0300

    removed getPokemons from server app.ts - irrelevant

--- server/src/app.ts
+++ server/src/app.ts
@@ -27,41 +27,6 @@ class Pokemon {
   }
 }
 
-async function getPokemons(x: number, n: number) {
-  let pokemonData: Pokemon[] = [];
-  for (let i = x; i <= n; i++) {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-    let json = await response.json();
-    let data = {
-      //name
-      name: json.name,
-      //front image
-      front_image: json.sprites.front_default,
-      //back image
-      back_image: json.sprites.back_default,
-      //abilites array
-      abilities: json.abilities
-        .map((arr) => arr.ability)
-        .map((ability) => ability.name),
-      //types array
-      types: json.types.map((arr) => arr.type).map((type) => type.name),
-      //stats array
-      stats: json.stats
-        .map((arr) => arr.stat)
-        .map(
-          (stat) =>
-            `${stat.name}: ${
-              json.stats.find((obj) => obj.stat.name === stat.name).base_stat
-            }`
-        ),
-      height: json.height,
-      weight: json.weight,
-    };
-    const pokemon = new Pokemon(data);
-    pokemonData.push(pokemon);
-  }
-  return pokemonData;
-}
 const filePath: string = path.join(__dirname, "../data/data.json");
 let pokemonData: Pokemon[] = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
